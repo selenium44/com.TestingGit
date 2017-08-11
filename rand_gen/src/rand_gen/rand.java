193877f4c5e4eb7ce5a1a2beaf9ff3c4fbbf1b1a
selenium44 <meena@Yogis-MacBook-Pro-2.local>
2017-08-10 22:20:07 -0400

    This is a random numebr generator code

---
+++ rand_gen/src/rand_gen/rand.java
@@ -0,0 +1,20 @@
+package rand_gen;
+
+import java.util.Scanner;
+
+public class rand {
+
+	
+	
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		String ip = sc.next();
+		
+		//System.out.println("Input is :"+ip);
+		
+		System.out.println("In main");
+	}
+
+
+}
